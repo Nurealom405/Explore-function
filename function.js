9b342073604287566c46d0f16fd9b4657c9627d7
Nurealom405 <nurborno405@gmail.com>
2023-08-21 22:48:21 +0600

    declear a fucntion

---
+++ function.js
@@ -0,0 +1,12 @@
+// var age = 17;
+
+// function declaration
+function startFan(){
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('press the switch');
+}
+
+
+// call the function
+startFan();
