79389ac1864a5cd9f8a6a569e7238057ca73a5c7
Nurealom405 <nurborno405@gmail.com>
2023-08-21 23:04:02 +0600

    function parameter

--- parameter.js
+++ parameter.js
@@ -1,6 +1,30 @@
+// function bringSingara(money){
+//     console.log('mama singara den');
+//     console.log(money);
+// }
+
+// bringSingara(300);
+
 function bringSingara(money){
+    console.log('eto taka disen?', money)
     console.log('mama singara den');
-    console.log(money);
+    
 }
 
-bringSingara(300);
+var taka = 500;
+var name = 'Nur';
+// bringSingara(name);
+
+function add(num1, num2){
+    console.log('goint to add: ', num1, num2);
+}
+
+// add(12, 13);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a+b+c+d+e;
+    console.log(sum);
+}
+sum(15, 10, 30, 10, 5);
+
